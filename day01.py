ebac489420e87af3697d7db784d52252cf64fcea
Nakyung Park <71192916+Nakyung-0729@users.noreply.github.com>
2022-06-14 19:59:46 -0700

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,25 @@
+"""
+    Day1 : My First Program
+    Version : 1.0
+    Created : 2022.5.12
+    Updated : 2022.5.12
+    Author : Nakyung Park
+"""
+print('nkpark')
+
+print('#' * 40)
+print('My First Program')
+print('#' * 40)
+
+my_name = 'nkpark'
+your_name = 'noname'
+my_name2 = 'nk "Dangerous" park'
+my_name3 = "nk 'Sleepy' park"
+
+print(my_name,your_name)
+
+statement1 = "this is \n very big"
+statement2 = "this is \t very big"
+print(statement1)
+print(statement2)
+
