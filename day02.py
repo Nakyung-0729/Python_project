dfca1942a9841c08eacbfb785171a6288102f400
Nakyung Park <71192916+Nakyung-0729@users.noreply.github.com>
2022-06-14 20:08:31 -0700

    Update day02.py

--- day02.py
+++ day02.py
@@ -19,31 +19,18 @@ print()
 print("Mary's cosmetics")
 print('Mary\'s cosmetics')
 
-# 3. 신씨가 "도둑이야"라고 소리쳤다.
+# 3. 신씨가 도둑이야 라고 소리쳤다.
 print('신씨가 "도둑이야"라고 소리쳤다.')
 print('신씨가 \"도둑이야\"라고 소리쳤다.')
 
-# 4. a = 'first', b = 'second'일 때 first second를 출력하시오
+# 4. when a = 'first', b = 'second', print 'first second'
 a = 'first'
 b = 'second'
 print(a + " " + b)
 print(a, b)
 
-# print(RED + "2.input" + END)
-# a = input()
-# print(a)
-# b = input("값을 입력하세요 : ")
-# print(b)
-# a, b = input("두 개의 값을 입력하세요 : ").split(" ")
-# c = a + b
-# a1 = float(a)
-# b1 = float(b)
-# c = int(a1) + int(b1)
-# d = float(a) + float(b)
-# print(a, b, "연산결과:", c, d)
-
 print(GREEN + '3.operator' + END)
-print('(1)산술연산자')
+
 a = 'My name'
 b = 'is None'
 print(a + ' ' + b)
@@ -52,16 +39,14 @@ d = 100
 print(c + ' ' + str(d))
 
 print('10 - 10.9 =', 10 - 10.9)
-# print('my name' - 'my') # 오류, 문자열의 뺄셈은 불가
 
-print('안녕' * 5)
-# print('ddd' / 3) # 오류, 문자열의 나눗셈은 불가
+print('Hi' * 5)
 
 print('10 // 3 ->', 10 // 3)
 print('10 % 3 ->', 10 % 3)
 print('5 ** 1.25 ->', 5 ** 1.25)
 
-print('(2)비교연산자')
+
 a = 30
 b = 20
 c = not (a > b)
